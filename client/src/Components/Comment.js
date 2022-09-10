d2fdec27f57d461961a1ccbbffdeea20345efdb0
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-09-10 23:02:42 +0000

    nested comments

--- client/src/Components/Comment.js
+++ client/src/Components/Comment.js
@@ -4,6 +4,9 @@ import { usePost } from "../contexts/PostContext";
 import { CommentList } from "./CommentList";
 import { useState } from "react";
 import "./component.css";
+import CommentForm from "./CommentForm";
+import { useAsyncFn } from "../hooks/useAsync";
+import comments from "../services/comments";
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "medium",
@@ -11,7 +14,19 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 });
 
 export function Comment({ id, message, user, createdAt }) {
-  const { getReply } = usePost();
+  const { post, getReply, createLocalComment } = usePost();
+  const createCommentFn = useAsyncFn(comments);
+  const [isReplying, setIsReplying] = useState(false);
+
+  function onCommentReply(message) {
+    return createCommentFn
+      .execute({ postId: post.id, message, parentId: id })
+      .then((comment) => {
+        setIsReplying(false);
+        createLocalComment(comment);
+      });
+  }
+
   const childComments = getReply(id);
   const [areChildrenHidden, setAreChildrenHidden] = useState(false);
   return (
@@ -28,21 +43,36 @@ export function Comment({ id, message, user, createdAt }) {
           <IconBtn Icon={FaHeart} aria-label="Like">
             2
           </IconBtn>
-          <IconBtn Icon={FaReply} aria-label="Reply" />
+          <IconBtn
+            onClick={() => setIsReplying((prev) => !prev)}
+            isActive={isReplying}
+            Icon={FaReply}
+            aria-label={isReplying ? "Cancel Reply" : "Reply"}
+          />
           <IconBtn Icon={FaEdit} aria-label="Edit" />
 
           <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
         </div>
       </div>
+      {isReplying && (
+        <div className="mt-1 ml-3">
+          <CommentForm
+            autoFocus
+            onSubmit={onCommentReply}
+            loading={createCommentFn.loading}
+            error={createCommentFn.error}
+          />
+        </div>
+      )}
       {childComments?.length > 0 && (
         <>
           <div
             className={`nested_comment_bar ${areChildrenHidden ? "hide" : ""}`}
           >
-            <div
-              className="nested-comments"
-              onClick={() => setAreChildrenHidden(true)}
-            >
+            <button className="btn" onClick={() => setAreChildrenHidden(true)}>
+              Hide Comments
+            </button>
+            <div className="nested-comments">
               <CommentList comments={childComments} />
             </div>
           </div>
